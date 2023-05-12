ff6f8e0b4ccb17f82a8cff5c11af405e70f6b0f5
Ruslan Feduleyev <8reflections@gmail.com>
2023-05-13 02:42:36 +0600

    First threads resolved

--- src/components/gifts/gifts.js
+++ src/components/gifts/gifts.js
@@ -1,17 +1,58 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const giftHorizontalItem = document.querySelector('.gifts__HorizontalItemsRow-item')
-
-  const clone1 = giftHorizontalItem.cloneNode(true)
-  const clone2 = giftHorizontalItem.cloneNode(true)
-  clone2.setAttribute('style', 'margin-top: 0')
-  clone1.setAttribute('style', 'margin: 0')
-  const arr = [clone1, clone2]
-
+  const giftsHorizontalRow = document.querySelector('.gifts__HorizontalItemsRow')
   const showAllGiftsButton = document.querySelector('.gifts__showAllButton')
-  showAllGiftsButton.addEventListener('click', e => {
-    arr.map(item => {
-      giftHorizontalItem.after(item)
-      showAllGiftsButton.remove()
-    })
+
+  /* Fetch gifts from data.json */
+  showAllGiftsButton.addEventListener('click', () => {
+    fetch('src/components/gifts/data.json')
+      .then(res => res.json())
+      .then(res => {
+        res.map(item => {
+
+          const renderAttributes = () => {
+            return(
+              item.attributes.map(attribute => {
+                return(
+                  `
+                  <p class="gifts__item-attributes-item">
+                    ${attribute.text}
+                  </p>
+                  `
+                )
+              }).join('')
+            )
+          }
+
+          giftsHorizontalRow.insertAdjacentHTML('beforeend', `
+          <a href=${item.link} class="gifts__item gifts__HorizontalItemsRow-item gifts__HorizontalItemsRow-itemRendered gifts__HorizontalItemsRow-itemRenderedActive">
+
+            <img src=${item.imageSrc}
+                 alt="comics"
+                 class="gifts__item-img gifts__HorizontalItemsRow-item-img">
+
+            <div class="gifts__HorizontalItemsRow-item-content">
+                <div class="gifts__item-title">
+                    ${item.title}
+                </div>
+
+                <div class="gifts__item-attributes">
+                    ${renderAttributes()}
+                </div>
+
+                <button class="fw-bold btn-main gifts__item-button gifts__item-buttonSimple">
+                    <span class="gifts__item-buttonSimple-text">
+                        ${item.buttonText}
+                    </span>
+                </button>
+            </div>
+          </a>
+          `)
+        })
+        showAllGiftsButton.setAttribute('style', 'display: none;')
+      })
+      .catch(e => {
+        throw new Error(e)
+      })
   })
+
 })
