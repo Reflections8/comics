e14c352f555e46aa03e0f03f719facee403afa5b
Ruslan Feduleyev <8reflections@gmail.com>
2023-05-17 15:27:25 +0600

    Mobile threads(2) resolved

--- src/components/ticker/ticker.js
+++ src/components/ticker/ticker.js
@@ -32,30 +32,40 @@ document.addEventListener('DOMContentLoaded', () => {
     }
   })
 
+  /* Click-event info */
+  let isClicked = false
+  let clickedPositionY = null
+  let topDifference = 0
+  let currentTopValue
+
   /* Pause column animation when mousedown event triggers */
-  const smallColumnWrapper = document.querySelector('.ticker__wrapper-smallColumn')
-  const largeColumnWrapper = document.querySelector('.ticker__wrapper-largeColumn')
 
   const eventListenerType = (() => navigator.maxTouchPoints ? ['touchstart', 'touchend'] : ['mousedown', 'mouseup'])()
   eventListenerType.map(eventType => {
-    document.addEventListener(eventType, (e) => {
+    moveContainer.addEventListener(eventType, (e) => {
+      const thisColumns = moveContainer.querySelectorAll('.ticker__wrapper-Column')
 
-      const thisColumn = e.target.closest('.ticker__wrapper-Column')
-      const thisColumnChildren = thisColumn?.children
-
-      if (thisColumn && eventType === 'mousedown' || thisColumn && eventType === 'touchstart') {
-        for (const column of thisColumnChildren) {
-          column.classList.add('animationPaused')
+      thisColumns.forEach((column) => {
+        if (eventType === 'mousedown' || eventType === 'touchstart') {
+          isClicked = true
+          clickedPositionY = e.pageY
+          for (const child of column.children) {
+            child.classList.add('animationPaused')
+          }
         }
-      }
 
-      if (thisColumn && eventType === 'mouseup' || thisColumn && eventType === 'touchend') {
-        for (const column of thisColumnChildren) {
-          column.classList.remove('animationPaused')
+        if (eventType === 'mouseup' || eventType === 'touchend') {
+          isClicked = false
+          topDifference = e.pageY - clickedPositionY
+          for (const child of column.children) {
+            child.classList.remove('animationPaused')
+          }
         }
-      }
+      })
 
       // TODO: add function, that resumes animation if mouseup/touchend was not on a column element
+      // TODO: resolve topDifference issue (smooth drag need)
+      // TODO: try to add function, that restarts keyframe animation each time mouseup/touchend event triggers, with start position equals corrected topDifference value.
 
     })
   })
@@ -67,7 +77,34 @@ document.addEventListener('DOMContentLoaded', () => {
             --small-ticker-animation-duration: ${smallColumn[0].offsetHeight / speed}s;
             --large-ticker-animation-duration: ${largeColumn[0].offsetHeight / speed}s;
         }
+        
+        @keyframes smallTickerAnimation {
+            0% {
+              top: 0;
+            }
+            
+            100% {
+              top: -${smallColumn[0].offsetHeight};
+            }
+        }
+
+        @keyframes largeTickerAnimation {
+            0% {
+              top: 0;
+            }
+            
+            100% {
+              top: -${largeColumn[0].offsetHeight};
+            }
+        }
     </style>
   `)
 
+  /* Swipe logic */
+  // moveContainer.addEventListener('mousemove', e => {
+  //   if (isClicked) {
+  //     moveContainer.style.top = `${(topDifference + (e.pageY - clickedPositionY))}px`
+  //
+  //   }
+  // })
 })
